02e3fb7216526c67e2c4393cf1088d4ad059acbe
LGL <Gen20000229@163.com>
2023-08-02 11:42:36 +0800

    two

--- src/main/java/lab18/Lab18Application.java
+++ src/main/java/lab18/Lab18Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Lab18Application {
 
     public static void main(String[] args) {
+        System.out.println("kkkkk");
         SpringApplication.run(Lab18Application.class, args);
     }
 
